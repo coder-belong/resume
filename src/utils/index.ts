352dee06b8a8c884fd132fdc8fd5f3da511e375e
lengyibai <1329670984@qq.com>
2023-02-22 17:50:25 +0800

    cancel: 取消重复动画

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,4 @@
-import { PRINT } from "@/config";
-
-export const $animateOnce = (el: HTMLElement, className: string) => {
+export default (el: HTMLElement, className: string) => {
   const observer = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
@@ -14,26 +12,3 @@ export const $animateOnce = (el: HTMLElement, className: string) => {
   );
   observer.observe(el);
 };
-
-export const $animateRepeat = (el: HTMLElement, className: string) => {
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting && entry.intersectionRatio >= 0.25) {
-          el.children[0].classList.remove(className);
-        } else if (!entry.isIntersecting) {
-          el.children[0].classList.add(className);
-        }
-      });
-    },
-    { threshold: [0, 0.25, 1] }
-  );
-  observer.observe(el);
-};
-let $animate = $animateRepeat;
-
-if (PRINT) {
-  $animate = $animateOnce;
-}
-
-export default $animate;
